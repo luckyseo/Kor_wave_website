1cbff3050575041a474df882f8ddff9609dd2ec6
luckyseo <ityun777@gmail.com>
2022-12-06 16:27:53 +0900

    made background

--- js/index.js
+++ js/index.js
@@ -6,10 +6,19 @@ const images=[
         "greeting/seoulNight.jpg",
         "greeting/metroRiver.jpg"
     ]
-const image = images[Math.floor(Math.random()*5)]
+const image = images[Math.floor(Math.random()*images.length)]
+console.log(image)
+const bgimg = document.querySelector("body")
+//bgimg.src = `img/${image}`
+bgimg.style.backgroundImage = `linear-gradient(to top right, black, transparent), url('img/${image}')`
+bgimg.style.backgroundRepeat=`no-repeat`
+bgimg.style.backgroundPosition= `center`
+bgimg.style.backgroundAttachment= `fixed `
+bgimg.style.backgroundSize = "cover";
 
-document.querySelector("#StartPage").setAttribute("background-image",`img/${image}`)
 
 
+//document.querySelector("#StartPage").setAttribute("background-image",`img/${image}`)
+console.log(bgimg.style.backgroundImage)
 
 
