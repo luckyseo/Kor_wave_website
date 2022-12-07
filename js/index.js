88dfd5fa8d2dd9bb43c0ed13f529ad2915ef0a33
luckyseo <ityun777@gmail.com>
2022-12-07 17:16:31 +0900

    add menu bar detail

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 const images=[
         "greeting/citystreet.jpg",
         "greeting/kbg.jpg",
-        "greeting/namsan.jpg",
+        "greeting/colorfulbkg.jpg",
         "greeting/seoulNight.jpg",
         "greeting/metroRiver.jpg"
     ]
@@ -10,7 +10,7 @@ const image = images[Math.floor(Math.random()*images.length)]
 console.log(image)
 const bgimg = document.querySelector("body")
 //bgimg.src = `img/${image}`
-bgimg.style.backgroundImage = `linear-gradient(to top right, black, transparent), url('img/${image}')`
+bgimg.style.backgroundImage = `linear-gradient(to top, rgba(0,0,0,0.9),rgba(0,0,0,0.8), rgba(0,0,0,0.5), rgba(0,0,0,0.4)), url('img/${image}')`
 bgimg.style.backgroundRepeat=`no-repeat`
 bgimg.style.backgroundPosition= `center`
 bgimg.style.backgroundAttachment= `fixed `
@@ -20,5 +20,4 @@ bgimg.style.backgroundSize = "cover";
 
 //document.querySelector("#StartPage").setAttribute("background-image",`img/${image}`)
 console.log(bgimg.style.backgroundImage)
-
 
