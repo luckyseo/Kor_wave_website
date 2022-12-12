652ce100f2bccc17f45022e628d1ceafd61a8fb9
luckyseo <ityun777@gmail.com>
2022-12-12 18:15:03 +0900

    almost fin

--- js/index.js
+++ js/index.js
@@ -34,3 +34,40 @@ function eraseUnderline(event){
 }
 navMenu.forEach((menu)=>(menu).addEventListener("mouseenter",showUnderline))
 navMenu.forEach((menu)=>(menu).addEventListener("mouseleave",eraseUnderline))
+
+
+const bar = document.querySelectorAll(".Asia-Oceania .bar");
+const bar2 =document.querySelectorAll(".America .bar");
+const bar3 =document.querySelectorAll(".EU .bar");
+const bar4 =document.querySelectorAll(".Africa .bar");
+
+const colorBar=["orange","red","violet","hotpink","darkblue"]
+for(let i=0; i<bar.length;i++){
+    let width = parseFloat(bar[i].getAttribute("data-percentage"))
+    bar[i].style.width = `${width}%`
+    bar[i].innerText = `${width}%`
+    bar[i].style.backgroundColor = colorBar[0]
+    bar[i].style.borderRadius = "0px 5px 5px 0px"
+}
+
+for(let i=0; i<bar2.length;i++){
+    let width = parseFloat(bar2[i].getAttribute("data-percentage"))
+    bar2[i].style.width = `${width}%`
+    bar2[i].innerText = `${width}%`
+    bar2[i].style.backgroundColor = colorBar[1]
+    bar2[i].style.borderRadius = "0px 5px 5px 0px"
+}
+for(let i=0; i<bar3.length;i++){
+    let width = parseFloat(bar3[i].getAttribute("data-percentage"))
+    bar3[i].style.width = `${width}%`
+    bar3[i].innerText = `${width}%`
+    bar3[i].style.backgroundColor = colorBar[2]
+    bar3[i].style.borderRadius = "0px 5px 5px 0px"
+}
+for(let i=0; i<bar4.length;i++){
+    let width = parseFloat(bar4[i].getAttribute("data-percentage"))
+    bar4[i].style.width = `${width}%`
+    bar4[i].innerText = `${width}%`
+    bar4[i].style.backgroundColor = colorBar[3]
+    bar4[i].style.borderRadius = "0px 5px 5px 0px"
+}
